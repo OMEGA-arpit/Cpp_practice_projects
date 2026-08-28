40cc8e8a956d5fbb27914c822de4f26586a83da1
Arpit Jain <arpit9660288717@gmail.com>
2026-08-28 07:51:03 +0530

    Removing build from remote

--- Music_player/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arpit_jain2001/Learning/Practise_Projects/Music_player
-# Build directory: /home/arpit_jain2001/Learning/Practise_Projects/Music_player/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MusicPlayerTests "/home/arpit_jain2001/Learning/Practise_Projects/Music_player/build/MusicPlayerTests")
-set_tests_properties(MusicPlayerTests PROPERTIES  _BACKTRACE_TRIPLES "/home/arpit_jain2001/Learning/Practise_Projects/Music_player/CMakeLists.txt;58;add_test;/home/arpit_jain2001/Learning/Practise_Projects/Music_player/CMakeLists.txt;0;")
-subdirs("googletest")
